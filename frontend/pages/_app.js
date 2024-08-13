c717226a7c020588d34e30a90f58c98425301efe
Shobitha-nayak <68498429+Shobitha-nayak@users.noreply.github.com>
2024-08-13 11:47:38 +0530

    _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,3 +1,23 @@
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import '../styles/globals.css';
+
+// function MyApp({ Component, pageProps }) {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     if (router.pathname === '/') {
+//       const isLoggedIn = localStorage.getItem('isLoggedIn');
+//       if (!isLoggedIn) {
+//         router.push('/login');
+//       }
+//     }
+//   }, [router]);
+
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import '../styles/globals.css';
